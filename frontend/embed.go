b9c40105bc67828af01a83730800e6d3f90fc8a0
zhoukailian <zhoukailian@meituan.com>
2026-03-25 14:13:47 +0800

    refactor: load frontend vendor assets from cdn

--- frontend/embed.go
+++ frontend/embed.go
@@ -4,5 +4,5 @@ import "embed"
 
 // StaticFiles 嵌入所有前端静态文件
 //
-//go:embed *.html img/* css/*.css js/*.js vendor/css/*.css vendor/js/*.js vendor/fonts/*
+//go:embed *.html img/* css/*.css js/*.js
 var StaticFiles embed.FS
